bb3fea69bdf9d9f68282cecc726a94857be05b08
Adriana <soto7adriana@gmail.com>
2026-06-23 23:26:39 -0400

    feat(navegacion): crear pantallas base

---
+++ app/src/main/java/com/raccy/racplanner/screens/DetalleCarrera.kt
@@ -0,0 +1,9 @@
+package com.raccy.racplanner.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DetalleCarrera() {
+    Text("Detalles de la carrera seleccionada")
+}
---
+++ app/src/main/java/com/raccy/racplanner/screens/OrganizadorHor.kt
@@ -0,0 +1,9 @@
+package com.raccy.racplanner.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun OrganizadorHor () {
+    Text("Grid con horarios")
+}
