a88e23da35ac5d4add0dc95e1e465ef954c77d66
Adriana <soto7adriana@gmail.com>
2026-06-27 18:17:57 -0400

    refactor(navigation): mover la navegacion a AppNav

--- app/src/main/java/com/raccy/racplanner/MainActivity.kt
+++ app/src/main/java/com/raccy/racplanner/MainActivity.kt
@@ -4,91 +4,35 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.raccy.racplanner.screens.Ajustes
-import com.raccy.racplanner.screens.BuscadorCarrera
-import com.raccy.racplanner.screens.DetalleCarrera
-import com.raccy.racplanner.screens.OrganizadorHor
-import com.raccy.racplanner.screens.PerfilUs
+import com.raccy.racplanner.navigation.AppNav
 import com.raccy.racplanner.ui.theme.RacPlannerTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
+
         setContent {
-            var pantalla by remember {
-                mutableStateOf("buscador")
-            }
-            var codCarrera by remember {
-                mutableStateOf("")
-            }
-            Scaffold { innerPadding ->
-                Column(
-                    modifier = Modifier.fillMaxSize().padding(innerPadding)
-                ) {
-                    Column(
-                        modifier = Modifier.weight(1f)
-                    ) {
-                        when (pantalla) {
-                            "buscador" -> BuscadorCarrera(
-                                cambiarPantalla = { pantalla = it },
-                                seleccionarCarrera = { codigo -> codCarrera = codigo })
-                            "organizador" -> OrganizadorHor(cambiarPantalla = { pantalla = it })
-                            "detalle" -> DetalleCarrera(
-                                cambiarPantalla = { pantalla = it },
-                                codigo = codCarrera)
-                            "perfil" -> PerfilUs(cambiarPantalla = { pantalla = it })
-                            "ajustes" -> Ajustes(cambiarPantalla = { pantalla = it })
-                        }
-                    }
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(8.dp),
-                        horizontalArrangement = Arrangement.SpaceEvenly
-                    ){
-                        Button(onClick = { pantalla = "buscador" }) {
-                            Text("Buscar")
-                        }
 
-                        Button(onClick = { pantalla = "organizador" }) {
-                            Text("Organizar")
-                        }
+            RacPlannerTheme {
 
-                        Button(onClick = { pantalla = "perfil" }) {
-                            Text("Perfil")
-                        }
+                AppNav()
 
-                        Button(onClick = { pantalla = "ajustes" }) {
-                            Text("Ajustes")
-                        }
-                    }
-                }
             }
+
         }
     }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
+    androidx.compose.material3.Text(
         text = "Hello $name!",
         modifier = modifier
     )
